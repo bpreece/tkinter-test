9e5143c0227838544edc8fbac45ae7d1ba37894e
Ben Preece <bpreece@comcast.net>
2025-03-01 16:37:01 -0600

    Dark theme

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 import tkinter as tk
 
 class App(tk.Tk):
@@ -5,11 +7,16 @@ class App(tk.Tk):
         super().__init__()
         self.title("Tkinter GUI Application")
         self.geometry("400x300")
+        self.configure(bg='black')  # Set the background to dark color
         self.create_widgets()
 
     def create_widgets(self):
-        label = tk.Label(self, text="Hello, Tkinter!")
+        label = tk.Label(self, text="Hello, Tkinter!", fg='white', bg='black')
         label.pack(pady=20)
+
+        # Add a button with blue as the thematic color
+        button = tk.Button(self, text="Click Me", fg='white', bg='blue')
+        button.pack(pady=10)
 
 if __name__ == "__main__":
     app = App()
